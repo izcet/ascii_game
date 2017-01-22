e33143c9f7ab7b78ae7b56fc68ec43dd01d8a75b
Isaac Rhett <isaacjrhett@gmail.com>
2017-01-21 17:01:27 -0800

    revised. check the /dev/event/input (path name?) for keyboard

--- src/main2.c
+++ src/main2.c
@@ -7,21 +7,22 @@
 
 int main(int argc, char **argv)
 {
-	int fd;
-	/*	if(argc < 2) {
-		printf("usage: %s <device>\n", argv[0]);
-		return 1;
+		int fd;
+		/*	if(argc < 2) {
+			printf("usage: %s <device>\n", argv[0]);
+			return 1;
+			}
+			fd = open(argv[1], O_RDONLY);*/
+		struct input_event ev;
+		//	printf("fd is %i\n", fd);*/
+		fd = open(argv[1], O_RDONLY);//stdin, O_RDONLY);
+		printf("fd is %i", fd);
+		while (1)
+		{
+				read(fd, &ev, sizeof(struct input_event));
+
+				if(ev.type == 1)
+						printf("key %i state %i\n", ev.code, ev.value);
+
 		}
-		fd = open(argv[1], O_RDONLY);*/
-	struct input_event ev;
-	//	printf("fd is %i\n", fd);*/
-//	fd = open(stdin, O_RDONLY);
-	while (1)
-	{
-		read(STDOUT_FILENO, &ev, sizeof(struct input_event));
-
-//		if(ev.type == 1)
-			printf("key %i state %i\n", ev.code, ev.value);
-
-	}
 }
